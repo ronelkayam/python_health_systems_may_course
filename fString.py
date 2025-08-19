792af64793416ac67730aeaa4899ab53312c296a
ron elkayam <ronelk6@gmail.com>
2025-08-19 19:20:40 +0300

    support

--- fString.py
+++ fString.py
@@ -1,5 +1,12 @@
-name = "Ron"
-age = 34
-city = "kiryat-gat"
-newLine = "\n"
-print(f"my name is:{name}{newLine}my age is:{age}{newLine}and i\'m living in:{city}")
+patient_name ="Noa"
+age = 40
+blood_type = "A+"
+heart_rate = 78
+temperature = 37.2
+new_line = "\n"
+print(f"Medical report:{new_line}"
+      f"name:{patient_name}{new_line}"
+      f"Age:{age}{new_line}"
+      f"Blood type:{blood_type}{new_line}"
+      f"Heart rate:{heart_rate} bpm{new_line}"
+      f"Body temperature:{temperature}")
